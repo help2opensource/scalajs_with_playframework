4b1a06291cba03d247cad220dc66a256febc474f
Ksawery Oleszyński <ksawery.oleszynski@gmail.com>
2020-12-21 20:15:24 +0100

    lesson 8

--- server/app/controllers/Login.scala
+++ server/app/controllers/Login.scala
@@ -11,8 +11,22 @@ class Login @Inject() (cc: ControllerComponents)
       Ok(views.html.login())
     }
 
-  def validate(login: String, password: String) =
+  def validateGet(login: String, password: String) =
     Action {
       Ok(s"Validated user: $login with password: $password")
+    }
+
+  def validatePost =
+    Action { request =>
+      request.body.asFormUrlEncoded
+        .map { args =>
+          {
+            val login = args.get("login")
+            val password = args.get("password")
+            println(s"Logged in user: $login with password $password")
+          }
+          Redirect(routes.TaskList1.taskList())
+        }
+        .getOrElse(Redirect(routes.Login.login()))
     }
 }
